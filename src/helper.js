b82b4b8e77f63f454fe89017d0060008ee9bf1bc
ScrUbieNoobie <jivancharis143@gmail.com>
2023-11-11 22:17:37 +0800

    added all api

--- src/helper.js
+++ src/helper.js
@@ -1,25 +1,10 @@
 const bcrypt = require('bcrypt');
 
-const getUserTypeId = (userType) => {
-	var user_type_id;
-	if (userType === "librarian") {
-		user_type_id = "a8adfa00-6680-49b3-bf94-caa8c3f1d823";
-	}
-	if (userType === "teacher") {
-		user_type_id = "a8adfa00-6680-49b3-bf94-caa8c3f1d822";
-	}
-	if (userType === "student") {
-		user_type_id = "a8adfa00-6680-49b3-bf94-caa8c3f1d822";
-	}
-	return user_type_id;
-}
-
 const cryptPassword = async (password) => {
 	const hash = await bcrypt.hash(password, 10);
 	return hash;
 };
 
 module.exports = { 
-	getUserTypeId,
 	cryptPassword
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const { db } = require('./db');
-const { getUserTypeId, cryptPassword } = require('./helper');
+const { cryptPassword } = require('./helper');
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = 5454;
@@ -13,22 +13,48 @@ var jsonParser = bodyParser.json();
 
 app.use(cors());
 
-app.get('/server-ping', (_req, res) => {
-	res.status(200).json({
-		response: 'server-pong'
+const apiCall = async (params, queryString, res) => {
+	client = await db();
+	try {
+		const query = {
+			text: queryString,
+			values: params
+		};
+		const data = await client.query(query);
+		if (data) {
+			return data;
+		}
+	} catch (e) {
+		error(e, res);
+	} finally {
+		if (client) {
+			client.release();
+		}
+	}
+};
+
+const error = (response, res) => {
+	res.status(500).json({
+		response: response.message
 	});
+};
+
+const success = (response, res) => {
+	res.status(200).json({
+		response: response
+	});
+};
+
+app.get('/api/server-ping', (_req, res) => {
+	success('server-pong', res);
 });
 
-app.get('/pg-ping', async (_req, res) => {
+app.get('/api/pg-ping', async (_req, res) => {
 	try {
 		client = await db();
-		return res.status(200).json({
-			response: client
-		});
+		success(client, res);
 	} catch (e) {
-		return res.status(500).json({
-			response: e
-		});
+		error(e, res);
 	} finally {
 		if (client) {
 			client.release();
@@ -36,44 +62,350 @@ app.get('/pg-ping', async (_req, res) => {
 	};
 });
 
-app.post('/sign-up', jsonParser, async (_req, res) => {
+// USER_SECTION
+
+// INSERT_USER
+app.post('/api/user', jsonParser, async (_req, res) => {
 	try {
-		client = await db();
-		
 		const userId = uuidv4();
-		
 		const { 
 			first_name,
 			middle_name,
 			last_name,
 			user_email,
 			user_password,
-			user_type
+			user_type_id
 		} = _req.body;
-
-		const user_type_id = getUserTypeId(user_type);
-
 		const password = await cryptPassword(user_password);
-
-		const query = {
-			text: "INSERT INTO lirra.user(user_id, first_name, middle_name, last_name, user_email, user_password, user_type_id) VALUES($1, $2, $3, $4, $5, $6, $7);",
-			values: [userId, first_name, middle_name, last_name, user_email, password, user_type_id]
-		};
-
-		await client.query(query);
-
-		return res.status(200).json({
-			response: 'success'
-		});
-
-	} catch (e) {
-		return res.status(500).json({
-			response: e.message
-		});
-	} finally {
-		if (client) {
-			client.release();
+		const queryString = `
+			INSERT INTO lirra.user(user_id, first_name, middle_name, last_name, user_email, user_password, user_type_id) 
+			VALUES($1, $2, $3, $4, $5, $6, $7);
+		`;
+		const params = [
+			userId, 
+			first_name, 
+			middle_name, 
+			last_name, 
+			user_email, 
+			password, 
+			user_type_id
+		];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
 		}
+	} catch (e) {
+		error(e, res);
+	}
+});
+
+// READ_USER
+
+app.get('/api/user/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const queryString = "SELECT * FROM lirra.user WHERE user_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success(result.rows, res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// UPDATE_USER
+
+app.patch('/api/user/:id', jsonParser, async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const { 
+			first_name,
+			middle_name,
+			last_name,
+			user_email,
+			user_type_id
+		} = _req.body;
+		const queryString = "UPDATE lirra.user SET first_name = $1, middle_name = $2, last_name = $3, user_email = $4, user_type_id = $5 WHERE user_id = $6;";
+		const params = [first_name, middle_name, last_name, user_email, user_type_id, id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// DELETE_USER
+
+app.delete('/api/user/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const queryString = "DELETE FROM lirra.user WHERE user_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// COURSE_SECTION
+
+// INSERT_COURSE
+
+app.post('/api/course', jsonParser, async (_req, res) => {
+	try {
+		const courseId = uuidv4();
+		const { course_name } = _req.body;
+		const queryString = "INSERT INTO lirra.course(course_id, course_name) VALUES($1, $2);";
+		const params = [courseId, course_name];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// READ_COURSE
+
+app.get('/api/course/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const queryString = "SELECT * FROM lirra.course WHERE course_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success(result.rows, res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// UPDATE_COURSE
+
+app.patch('/api/course/:id', jsonParser, async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const { course_name } = _req.body;
+		const queryString = "UPDATE lirra.course SET course_name = $1 WHERE course_id = $2;";
+		const params = [course_name, id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// DELETE_COURSE
+
+app.delete('/api/course/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const queryString = "DELETE FROM lirra.course WHERE course_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// LIBRARY_RESOURCE_SECTION
+
+// INSERT_LIBRARY_RESOURCE
+
+app.post('/api/library-resource', jsonParser, async (_req, res) => {
+	try {
+		const libResourceId = uuidv4();
+		const {
+			title,
+			author,
+			date_published,
+			place_published,
+			publisher,
+			keywords,
+			description,
+			recommendation_count,
+			in_library,
+			metadata,
+			course_id,
+			resourse_type_id
+		} = _req.body;
+		const queryString = `
+			INSERT INTO
+			lirra.library_resource(library_resource_id, title, author, date_published, place_published, publisher, keywords, description, recommendation_count, in_library, metadata, resource_type_id, course_id)
+			VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);
+		`;
+		const params = [
+			libResourceId,
+			title,
+			author,
+			date_published,
+			place_published,
+			publisher,
+			keywords,
+			description,
+			recommendation_count,
+			in_library,
+			metadata,
+			resourse_type_id,
+			course_id
+		];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// READ_LIBRARY_RESOURCE
+
+app.get('/api/library-resource/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const queryString = "SELECT * FROM lirra.library_resource WHERE library_resource_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success(result.rows, res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// UPDATE_LIBRARY_RESOURCE
+
+app.patch('/api/library-resource/:id', jsonParser, async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const {
+			title,
+			author,
+			date_published,
+			place_published,
+			publisher,
+			keywords,
+			description,
+			recommendation_count,
+			in_library,
+			metadata,
+		} = _req.body;
+		const queryString = `
+			UPDATE lirra.library_resource
+			SET title = $1, author = $2, date_published = $3, place_published = $4, publisher = $5, keywords = $6, description = $7, recommendation_count = $8, in_library = $9, metadata = $10
+			WHERE library_resource_id = $11;
+		`;
+		const params = [
+			title,
+			author,
+			date_published,
+			place_published,
+			publisher,
+			keywords,
+			description,
+			recommendation_count,
+			in_library,
+			metadata,
+			id,
+		];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// DELETE_LIBRARY_RESOURCE
+
+app.delete('/api/library-resource/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id;
+		const queryString = "DELETE FROM lirra.library_resource WHERE library_resource_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// RECOMMENDATION_SECTION
+
+// INSERT RECOMMENDATION
+
+app.post('/api/recommendation', jsonParser, async (_req, res) => {
+	try {
+		const recommendationId = uuidv4();
+		const {
+			user_id,
+			library_resource_id
+		} = _req.body;
+		const queryString = `
+			INSERT INTO lirra.recommendation(recommendation_id, user_id, library_resource_id)
+			VALUES($1, $2, $3);
+		`;
+		const params = [recommendationId, user_id, library_resource_id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// READ_RECOMMENDATION
+
+app.get('/api/recommendation/:id', async (_req, res) => {
+	try {
+		const id = _req.params.id
+		const queryString = "SELECT * FROM lirra.recommendation WHERE recommendation_id = $1;";
+		const params = [id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success(result.rows, res);
+		}
+	} catch (e) {
+		error(e, res);
+	};
+});
+
+// UPDATE_READ_RECOMMENDATION
+
+app.patch('/api/recommendation/:id', jsonParser, async (_req, res) => {
+	try {
+		const id = _req.params.id
+		const {
+			user_id,
+			library_resource_id
+		} = _req.body;
+		const queryString = "UPDATE lirra.recommendation SET user_id = $1, library_resource_id = $2 WHERE recommendation_id = $3;";
+		const params = [user_id, library_resource_id, id];
+		const result = await apiCall(params, queryString, res);
+		if (result) {
+			success('success', res);
+		}
+	} catch (e) {
+		error(e, res);
 	};
 });
 
